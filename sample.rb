653ce12dc120630772994684550f27e1f624fc9e
KBY7777777 <koba.yu.7777777@gmail.com>
2020-07-26 21:18:30 +0900

    replaced the massage in sample.rb

--- sample.rb
+++ sample.rb
@@ -6,7 +6,10 @@ def hello
   # p "hello, Git."
 
   #応用カリNo74の実演にて、さらに下記のとおり書き換え
-  p "hello, GitHub."
+  # p "hello, GitHub."
+
+  #応用カリNo75の実演にて、さらに下記のとおり書き換え
+  p "hello, GitHub Desktop."
 
 end
 
